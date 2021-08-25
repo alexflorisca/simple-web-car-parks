dee6da1ff67901fb54521e6a398ed78564cdd9da
Alex Florisca <alexflorisca@gmail.com>
2021-08-25 23:53:42 +0100

    Pagination

--- App.js
+++ App.js
@@ -1,35 +1,71 @@
 import React, { useEffect, useState } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
-import { API_KEY } from '@env'
+import { ActivityIndicator, FlatList, Pressable, StyleSheet, Text, View } from 'react-native';
+import { API_URL, API_KEY } from '@env'
+import { decode } from 'html-entities';
 
 export default function App() {
   const [carParks, setCarParks] = useState([]);
+  const [pageLinks, setPageLinks] = useState([]);
+  const [isError, setError] = useState(false);
+  const [isLoading, setLoading] = useState(true);
 
-  const getCarParks = () => {
+
+  const onPagePress = (item) => {
+    getCarParks(item.url);
+  }
+
+  const getCarParks = (url) => {
+    console.log(url);
     if(!API_URL || !API_KEY) {
       throw new Error('Missing API_URL or API_KEY');
     }
 
-    fetch(API_URL, {
+    fetch(url, {
       headers: {
         'accept': 'application/json',
         'x-api-key': API_KEY
       }
     })
     .then(response => response.json())
-    .then(results => setCarParks(results))
-    .catch(error => console.error(error));
+    .then(results => {
+      setCarParks(results.data);
+      setPageLinks(results.links);
+    })
+    .catch(error => setError(true))
+    .finally(() => setLoading(false));
   }
 
   useEffect(() => {
-    getCarParks();
+    getCarParks(API_URL);
   }, []);
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Car Parks</Text>
-      <FlatList data={carParks.data} renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}>
-
+      { isLoading && <ActivityIndicator />}
+      <FlatList 
+        keyExtractor={item => item.id} 
+        data={carParks} 
+        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}>
+      </FlatList>
+      <FlatList 
+        keyExtractor={item => item.id}
+        horizontal 
+        data={pageLinks} 
+        renderItem={({ item }) => (
+          <Pressable style={styles.pagination} onPress={() => getCarParks(item.url)}>
+            <Text>{decode(item.label)}</Text>
+          </Pressable>
+        )}
+        ItemSeparatorComponent={
+        (({ highlighted }) => (
+          <View
+            style={[
+              styles.separator,
+              highlighted && { marginLeft: 0 }
+            ]}
+          />
+        ))}>
       </FlatList>
     </View>
   );
@@ -48,5 +84,12 @@ const styles = StyleSheet.create({
     padding: 10,
     fontSize: 18,
     height: 44,
+  },
+  separator: {
+    marginLeft: 3,
+    marginRight: 3
+  },
+  pagination: {
+
   }
 });
